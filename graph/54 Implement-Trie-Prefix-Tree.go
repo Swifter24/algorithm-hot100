05f7f89a8c571870c98ac7f8b30cb2bb0de52517
zwz <1915967609@qq.com>
2025-04-24 13:35:54 +0800

    graph

---
+++ graph/54 Implement-Trie-Prefix-Tree.go
@@ -0,0 +1,79 @@
+package graph
+
+//Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补全和拼写检查。
+//请你实现 Trie 类：
+//Trie() 初始化前缀树对象。
+//void insert(String word) 向前缀树中插入字符串 word 。
+//boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+//boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+//
+//示例：
+//输入
+//["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+//[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+//输出
+//[null, null, true, false, true, null, true]
+//
+//解释
+//Trie trie = new Trie();
+//trie.insert("apple");
+//trie.search("apple");   // 返回 True
+//trie.search("app");     // 返回 False
+//trie.startsWith("app"); // 返回 True
+//trie.insert("app");
+//trie.search("app");     // 返回 True
+
+type Trie struct {
+	root *Node
+}
+
+type Node struct {
+	son [26]*Node
+	end bool
+}
+
+func Constructor() Trie {
+	return Trie{&Node{}}
+}
+
+func (this *Trie) Insert(word string) {
+	cur := this.root
+	for _, c := range word {
+		c -= 'a'
+		if cur.son[c] == nil {
+			cur.son[c] = &Node{}
+		}
+		cur = cur.son[c]
+	}
+	cur.end = true
+}
+
+func (this *Trie) find(word string) int {
+	cur := this.root
+	for _, c := range word {
+		c -= 'a'
+		if cur.son[c] == nil {
+			return 0
+		}
+		cur = cur.son[c]
+	}
+	if cur.end {
+		return 2
+	}
+	return 1
+}
+func (this *Trie) Search(word string) bool {
+	return this.find(word) == 2
+}
+
+func (this *Trie) StartsWith(prefix string) bool {
+	return this.find(prefix) != 0
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
