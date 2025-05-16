c3f374cacfd22b8e58ef11d126420ed5a098f483
zwz <1915967609@qq.com>
2025-05-16 21:20:19 +0800

    dynamicProgramming

---
+++ dynamicProgramming/84 Perfect-Squares.go
@@ -0,0 +1,39 @@
+package dynamicProgramming
+
+import "math"
+
+//给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
+//
+//完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+//
+//示例 1：
+//输入：n = 12
+//输出：3
+//解释：12 = 4 + 4 + 4
+//
+//示例 2：
+//输入：n = 13
+//输出：2
+//解释：13 = 4 + 9
+//
+//提示：
+//1 <= n <= 104
+
+const N = 10001
+
+var f [N + 1]int
+
+func init() {
+	for i := 1; i <= N; i++ {
+		f[i] = math.MaxInt
+	}
+	for i := 1; i*i <= N; i++ {
+		for j := i * i; j <= N; j++ {
+			f[j] = min(f[j], f[j-i*i]+1)
+		}
+	}
+}
+
+func numSquares(n int) int {
+	return f[n]
+}
